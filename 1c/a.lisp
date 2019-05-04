bfdc3f91597c85ca2b126928684df57d70378483
Simon Leinen (SWITCH) <simon.leinen@switch.ch>
2019-05-04 22:24:16 +0200

    Add termination condition

--- 1c/a.lisp
+++ 1c/a.lisp
@@ -17,7 +17,7 @@
         (dotimes (k (length ptext))
           (push (aref ptext k) prog))
         (push (nreverse prog) progs)))
-    (let ((result (solve-1 '() progs)))
+    (let ((result (solve-1 n '() progs)))
       (if result
           (format t "Case #~D: ~{~A~}~%" (1+ caseno) result)
           (format t "Case #~D: IMPOSSIBLE~%" (1+ caseno))))))
@@ -31,23 +31,22 @@
 (defun rotate (prog)
   (append (rest prog) (list (first prog))))
 
-(defun solve-1 (pref progs)
-  (let ((first-set (remove-duplicates (mapcar #'first progs))))
-    (case (length first-set)
-      ((3) nil)
-      ((2)
-       (let ((feasible
-	      (first (remove-if #'(lambda (x)
-                                    (some #'(lambda (y)
-                                              (beatsp y x))
-					  first-set))
-				first-set))))
-         (solve-1 (cons feasible pref)
-		  (mapcar #'rotate
-			  (remove-if #'(lambda (x) (beatsp feasible (first x)))
-			     progs)))))
-      ((1)
-       (let ((winner (beater (first first-set))))
-	 (reverse (cons winner pref)))))))
+(defun solve-1 (n pref progs)
+  (and (>= n 0)
+       (let ((first-set (remove-duplicates (mapcar #'first progs))))
+	 (case (length first-set)
+	   ((3) nil)
+	   ((2)
+	    (let ((feasible
+		   (first (intersection first-set (mapcar #'beater first-set)))))
+              (solve-1
+	       (1- n)
+	       (cons feasible pref)
+	       (mapcar #'rotate
+		       (remove-if #'(lambda (x) (beatsp feasible (first x)))
+				  progs)))))
+	   ((1)
+	    (let ((winner (beater (first first-set))))
+	      (reverse (cons winner pref))))))))
 
 (solve)
